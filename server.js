c186f65aea2327243fc2fc091323b86fc74dd7cf
SQaDRo-work <sqadro@yandex.ru>
2020-07-18 11:43:02 +0500

    Базовая настройка
    Node.js + express под одностраничник с ассетами стилей и скриптов
    Бутстрап стили и скрипты с CDN + прикрутил тему что видел в примере реальной задачи (https://getbootstrap.com/docs/4.5/examples/dashboard/)

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.get('/', function(req, res) {
+  res.sendfile('views/index.html');
+});
+
+app.listen(8080);
