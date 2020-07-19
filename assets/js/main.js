3c93b8a2346609b8371488e0a3371662bccc1be2
SQaDRo-work <sqadro@yandex.ru>
2020-07-19 20:13:19 +0500

    Chart types
    Селектор выбора типа графика (точнее параметра который показывается в динамике во времени)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,13 @@ let chart = new Chart;
 data_extractor.get_one_call_data();
 
 function change_city(select) {
-    var new_city = select.options[select.selectedIndex].value;
+    let new_city = select.options[select.selectedIndex].value;
     data_extractor.query_params.q = new_city;
     data_extractor.get_one_call_data();
+}
+
+function change_chart_type(select) {
+    let chart_type = select.options[select.selectedIndex].value;
+    chart.config.chart_column = chart_type;
+    chart.build_chart();
 }
