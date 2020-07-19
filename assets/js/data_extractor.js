97e6551076edd0741ee6213dd999e1ff14bbfbd3
SQaDRo-work <sqadro@yandex.ru>
2020-07-19 21:10:24 +0500

    Protocol
    Нашёл в документации корректный endpoint для защищённого соединения

--- assets/js/data_extractor.js
+++ assets/js/data_extractor.js
@@ -1,6 +1,6 @@
 class DataExtractor {
     constructor() {
-        this.end_point = "api.openweathermap.org";
+        this.end_point = "https://api.openweathermap.org";
         this.query_params = {
             q: "Moscow",
             units: "metric",
@@ -17,7 +17,7 @@ class DataExtractor {
     get_one_call_data() {
         let self = this;
         $.ajax({
-            url: `http://${this.end_point}/data/2.5/forecast`,
+            url: `${this.end_point}/data/2.5/forecast`,
             method: "GET",
             data: this.query_params,
             dataType: "json",
